bbe1c82e1e835ffa10772d45237c8d9757016afb
Kurihara Jun <lh182051+src@gmail.com>
2020-07-18 12:47:54 +0900

    Add initi form
    This commit is to add the initial form.

---
+++ src/components/ApplicationHeader.vue
@@ -0,0 +1,27 @@
+<template>
+  <header>
+    <div>LT大会登録フォーム</div>
+  </header>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({});
+</script>
+
+<style scoped>
+header {
+  margin: 0;
+  padding: 0;
+  background-color: cyan;
+  position: relative;
+  top: 0;
+  width: 100%;
+  height: 100%;
+}
+
+div {
+  margin: 0.25em;
+  font-size: 2rem;
+}
+</style>
---
+++ src/components/RegistForm.vue
@@ -0,0 +1,141 @@
+<template>
+  <div class="form-area">
+    <div class="form-wrap">
+      <div class="form-label">社員番号</div>
+      <div>
+        <input type="text" v-model="form.employeeNumber" />
+      </div>
+    </div>
+    <div class="form-wrap">
+      <div class="form-label">参加種別</div>
+      <div>
+        <div>
+          <input
+            type="radio"
+            name="participation"
+            id="participation-1"
+            value="1"
+            v-model="form.participationType"
+          />
+          <label for="participation-1">発表者</label>
+        </div>
+        <div>
+          <input
+            type="radio"
+            name="Participation"
+            id="Participation-2"
+            value="2"
+            v-model="form.participationType"
+          />
+          <label for="participation-2">参加のみ</label>
+        </div>
+      </div>
+    </div>
+    <div class="form-wrap">
+      <div class="form-label">氏名</div>
+      <div>
+        <input type="text" v-model="form.name" />
+      </div>
+    </div>
+    <div class="form-wrap">
+      <div class="form-label">LT大会に期待すること</div>
+      <div>
+        <textarea name id cols="30" rows="10" v-model="form.expect"></textarea>
+      </div>
+    </div>
+    <div class="form-wrap save-area">
+      <div>
+        <button class="btn-save">登録</button>
+      </div>
+      <div>
+        <button class="btn-clear">クリア</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from "vue-property-decorator";
+
+export interface FormModel {
+  employeeNumber: string;
+  participationType: number;
+  name: string;
+  expect: string;
+}
+
+@Component
+export default class DetailComponent extends Vue {
+  /**
+   *
+   */
+  constructor() {
+    super();
+    this.form = {
+      employeeNumber: "",
+      participationType: 2,
+      name: "",
+      expect: ""
+    };
+  }
+  @Prop() private form: FormModel;
+}
+</script>
+
+<style>
+.form-area {
+  margin: 10px auto;
+  width: 80%;
+  box-shadow: 4px 4px 4px 3px gray;
+  padding: 1em 0;
+  border-radius: 5px;
+}
+
+.form-wrap {
+  display: grid;
+  grid-template-columns: 1fr 2fr;
+  padding: 10px auto;
+  vertical-align: middle;
+  margin: auto auto;
+}
+
+.form-label {
+  margin: auto 10px;
+  text-align: right;
+}
+.save-area {
+  grid-template-columns: 1fr 1fr;
+  margin-top: 20px;
+}
+
+.btn-save,
+.btn-clear {
+  padding: 10px 20px;
+  color: #ffffff;
+  border: 0;
+}
+
+.btn-save {
+  background-color: #6adef6;
+}
+.btn-clear {
+  background-color: #f89c72;
+}
+
+.btn-save:focus,
+.btn-save:hover,
+.btn-clear:focus,
+.btn-clear:hover {
+  color: rgba(255, 255, 255, 10);
+}
+
+.btn-save:focus,
+.btn-save:hover {
+  background-color: #42c6e0;
+}
+
+.btn-clear:focus,
+.btn-clear:hover {
+  background-color: #cb5c2a;
+}
+</style>
