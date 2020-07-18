24c535539f1384c45eedfc2265409663e92246d2
Kurihara Jun <lh182051+src@gmail.com>
2020-07-18 13:05:33 +0900

    Add initi form (#24)
    * Add initi form
    
    This commit is to add the initial form.
    
    * Change GitHub Actions
    
    This commit is to change GitHub Actions which remote test process

---
+++ src/components/DetailComponent.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="center">
+    <div class="content">
+      <div>LT大会の詳細を記載する</div>
+      <div class="center">
+        <table class="time-schedule">
+          <thead>
+            <tr>
+              <th>時間</th>
+              <th>タイトル</th>
+              <th>担当者</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>19:00〜</td>
+              <td></td>
+              <td></td>
+            </tr>
+            <tr>
+              <td>19:10〜</td>
+              <td></td>
+              <td></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class DetailComponent extends Vue {}
+</script>
+
+<style scoped>
+div.center {
+  margin: auto;
+  width: 80vw;
+}
+div.content {
+  box-shadow: 4px 4px 4px 3px gray;
+  padding: 1em;
+  border-radius: 5px;
+}
+
+.time-schedule {
+  margin: 1rem auto;
+  width: 60%;
+}
+tr > th,
+tr > td {
+  border-bottom: 1px solid gray;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <application-header />
+    <detail-component />
+    <regist-form />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import ApplicationHeader from "@/components/ApplicationHeader.vue";
+import DetailComponent from "@/components/DetailComponent.vue";
+import RegistForm from "@/components/RegistForm";
+
+export default {
+  name: "Home",
+  components: {
+    ApplicationHeader,
+    DetailComponent,
+    RegistForm
+  }
+};
+</script>
